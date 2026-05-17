f1359b7b5f4809f1127672d7902ab600286e2efb
RehanA988 <rehanayyub027@gmail.com>
2026-05-17 18:48:38 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+st.title("🌿 Plant Disease Detection App")
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("plant_disease_model.keras")
+
+model = load_model()
+
+class_names = ["Healthy", "Diseased"]
+
+uploaded_file = st.file_uploader("Upload Leaf Image")
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, use_container_width=True)
+
+    img = image.resize((224, 224))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+    class_index = np.argmax(prediction)
+
+    st.success(f"Prediction: {class_names[class_index]}")

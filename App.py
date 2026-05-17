e9b4efa2091e9b3eae3f85098257001216378c29
RehanA988 <rehanayyub027@gmail.com>
2026-05-17 17:51:54 +0500

    Update App.py

--- App.py
+++ App.py
@@ -1,30 +1,11 @@
 import streamlit as st
-
-import numpy as np
 from PIL import Image
 
-st.title("🌿 Plant Disease Detection App")
+st.title("🌿 Plant Disease App (Test Mode)")
 
-@st.cache_resource
-def load_model():
-    model = tf.keras.models.load_model("plant_disease_model.keras")
-    return model
-
-model = load_model()
-
-class_names = ["Healthy", "Diseased"]
-
-uploaded_file = st.file_uploader("Upload Leaf Image", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload Image")
 
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, use_container_width=True)
-
-    img = image.resize((224, 224))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-
-    prediction = model.predict(img)
-    class_index = np.argmax(prediction)
-
-    st.success(f"Prediction: {class_names[class_index]}")
+    st.image(image)
+    st.success("App working fine ✅")
